3412ac4228068a630336ed978c6618c82128c196
Diablo <daniel.lomholt.2000@gmail.com>
2025-12-14 10:22:01 +0100

    Add simple test for macos and linux on pull request

---
+++ RLexamples/test.h
@@ -0,0 +1,28 @@
+Ritz_conv 0.001
+Zero_vec_length 0.001
+Mode 0
+Unimode 0
+Mode 0
+M start 0
+M end 0
+Number of spins 2
+Number of Couplings 2
+Number of Coupling strength 1
+Number of Hardcoded symmetries 0
+Number of custom symmetries 2
+Number of chosen GS q-values 0
+Number of Dimensions 1
+Translation vector 1
+Translation indices 1 0 0
+Number of spins in unit cell 1
+Relative position 0 0 0
+Qmax translation 1 1 1
+Spin position 0
+Spin position 1
+Construct symmetries 0
+Hardcoded symmetries 0
+Custom symmetry 0 1
+Custom symmetry 1 0
+Coupling strength vector -1 -1 0
+Coupling vector 0 1 0
+Coupling vector 1 0 0
