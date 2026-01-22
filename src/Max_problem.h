c567a984e34bf0a9d67725363c22ec5092e8d69d
Diablo <daniel.lomholt.2000@gmail.com>
2026-01-22 12:04:01 +0100

    Move max buffersize to max problem

--- src/Max_problem.h
+++ src/Max_problem.h
@@ -4,14 +4,15 @@
 // The following parameters define the maximal problem size that is
 // allowed when compiling the program. This is for optimization reasons.
 
-#define NCOUP 400        // Max number of couplings
-#define NCOUPSTR 10      // Maximal number of different coupling strengths
-#define NSPINS 100       // Maximum number of spins
-#define NSYM 20          // Maximum number of symmetries
-#define NSYMADD 10       // Maximum needs a description
-#define NUNIQUE 4500     // Needs a descriotion
-#define MAXARRAYSIZE 200 // max number of entries on a given line in the input file
-#define NRING 100        // Needs a description
-#define NRINGSTR 10      // Maximum number of different ring strengths
+#define NCOUP 400          // Max number of couplings
+#define NCOUPSTR 10        // Maximal number of different coupling strengths
+#define NSPINS 100         // Maximum number of spins
+#define NSYM 20            // Maximum number of symmetries
+#define NSYMADD 10         // Maximum needs a description
+#define NUNIQUE 4500       // Needs a descriotion
+#define MAXARRAYSIZE 200   // max number of entries on a given line in the input file
+#define NRING 100          // Needs a description
+#define NRINGSTR 10        // Maximum number of different ring strengths
+#define BUFFERSIZE 4194304 // Set up size of buffer for sparse matrix read/write
 
 #endif
